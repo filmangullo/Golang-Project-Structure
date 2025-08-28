e1d196a7f85db64006028b186e4ce708a2ef9a55
filmangullo <filmantapgullo@gmail.com>
2025-08-28 13:39:51 +0700

    update response

--- app/controllers/ArticleController/service.go
+++ app/controllers/ArticleController/service.go
@@ -1,20 +1,8 @@
 package ArticleController
 
-import (
-	"fmt"
-	"your_project_name/utils/DatetimeFunctions"
-	"your_project_name/utils/StringsFunctions"
-)
-
 func WelcomeService(input PostRequest) (*PostRequest, error) {
 	var response PostRequest
 
-	fmt.Println(DatetimeFunctions.FormatDateStyle("2025-07-19 00:00:00", "Y-m-d"))
-	fmt.Println(StringsFunctions.ToInt8("42"))
-	fmt.Println(StringsFunctions.ToInt16("42"))
-	fmt.Println(StringsFunctions.ToInt32("42"))
-	fmt.Println(StringsFunctions.ToInt64("42"))
-	fmt.Println(StringsFunctions.ToInt("42"))
 	return &response, nil
 }
 
